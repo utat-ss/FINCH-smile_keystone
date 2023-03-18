59745b11e502b641e5f99dfd0f668b34783d23e7
ThisisShoo <shuhan_zheng@qq.com>
2023-03-18 04:03:28 -0400

    Final polish for the week

--- smile/main.py
+++ smile/main.py
@@ -36,6 +36,7 @@ g_num_shifts_1D = 5
 g_shift_increment = .2 #nanometers
 g_total_shifts = int(2*(g_num_shifts_1D/g_shift_increment)+1)
 
+data_folder_path = 'data/TempData/'
 
 # Function imports (DEPRECATED)
   # from create_ref_and_test_spectra import create_ref_and_test_spectra
@@ -64,42 +65,47 @@ if __name__ == '__main__':
     # Globals further defined here in case we want to pass in other data
 
     # # # Quantification
-    column_averaged_spectra = data_matrix_collapse(data) # Step 1: Generate Column Averaged Spectra.
+    # Step 1: Generate Column Averaged Spectra.
+    column_averaged_spectra = data_matrix_collapse(data)
     print ("Step 1 Done, no issues.")
-    demo_SRF = test_spectral_response # Step 2: Generate SRFs.
+    np.savez_compressed(f'{data_folder_path}column_averaged_spectra', cas = column_averaged_spectra)
+
+    # Step 2: Generate SRFs.
+    demo_SRF = test_spectral_response
     print ("Step 2 Done, no issues.")
-    ref_spectra, test_spectra = create_ref_and_test_spectra(column_averaged_spectra, demo_SRF, wavelength_input, g_num_of_bands, g_num_shifts_1D, g_shift_increment, reference_spectra=MODTRAN_data) # Step 3, 4: Generate Reference and Test spectra. (0,100) is also a placeholder 
+
+    # Step 3, 4: Generate Reference and Test spectra. (0,100) is also a placeholder
+    ref_spectra, test_spectra = create_ref_and_test_spectra(column_averaged_spectra, demo_SRF, wavelength_input, g_num_of_bands, g_num_shifts_1D, g_shift_increment, reference_spectra=MODTRAN_data)
     # TODO: Add MODTRAN to this function as the reference spectra.
     print ("Step 3, 4 Done, no issues.")
-    sa_deg = spectral_angle_calculation(test_spectra[0], ref_spectra[0], g_data_dim) # Step 5: Calculate spectral angle from test and reference spectra.
-    print ("Step 5 Done, no issues.")
-    min_spectral_angle = determine_min_sa(sa_deg, g_data_dim) # Step 6: Determine minimum spectral angle.
-    print ("Step 6 Done, no issues.")
+    np.savez_compressed(f'{data_folder_path}ref_and_test_spectra', ref = ref_spectra, test = test_spectra)
 
-    print("Quantification complete, no issues.")
+    # Step 5: Calculate spectral angle from test and reference spectra.
+    sa_deg = spectral_angle_calculation(test_spectra[0], ref_spectra[0], g_data_dim)
+    print ("Step 5 Done, no issues.")
+    np.savez_compressed(f'{data_folder_path}sa_deg', sa_deg = sa_deg)
+
+    # Step 6: Determine minimum spectral angle.
+    min_spectral_angle = determine_min_sa(sa_deg, g_data_dim)
+    print ("Step 6 Done, no issues.")
+    np.savez_compressed(f'{data_folder_path}min_spectral_angle', msa = min_spectral_angle)
+
+    print(f"Quantification complete, no issues. Data saved to {data_folder_path}")
+
 
     # # # Correction
     # Step 7: Apply reverse of Quantified shifts to SRFs. DEPRECATED. STEP 7 IS NOW BUNDLED INTO STEP 9.
-    # reverse_shifted_SRFS = reverse_shift(data, demo_SRF, min_spectral_angle) 
-    spectra_wav, spectra_rad = spline_interpolation_all(data, wavelength_input, wavelength_increment_input, g_data_dim, wavelength) # Step 8: Spline interpolation of test spectra. INPUTS ALSO MAY NOT BE CORRECT. TODO: Check if imputs are still not correct.
+    # reverse_shifted_SRFS = reverse_shift(data, demo_SRF, min_spectral_angle)
+
+    # Step 8: Spline interpolation of test spectra. INPUTS ALSO MAY NOT BE CORRECT. TODO: Check if imputs are still not correct.
+    spectra_wav, spectra_rad = spline_interpolation_all(data, wavelength_input, wavelength_increment_input, g_data_dim, wavelength)
     print("Step 8 Done, no issues.")
+    np.savez_compressed(f'{data_folder_path}spline_interpolated', wav = spectra_wav, rad = spectra_rad)
+
     # Step 9: Generate smile corrected spectra for each pixel. (This turns out to be the most computationally expensive step.)
     corrected_datacube = smile_correction(spectra_rad, min_spectral_angle, test_spectral_response, g_num_of_bands, g_shift_increment, wavelength)
-
-    print("Correction complete, no issues.")
-
-    # # # Save the corrected data to data/TempData
-    folder_path = 'data/TempData/'
-    # Quantification data
-    np.save(f'{folder_path}column_averaged_spectra.npy', column_averaged_spectra)
-    np.save(f'{folder_path}ref_and_test_spectra.npy', [ref_spectra, test_spectra])
-    np.save(f'{folder_path}sa_deg.npy', sa_deg)
-    np.save(f'{folder_path}min_spectral_angle.npy', min_spectral_angle)
-    # Correction data
-    np.save(f'{folder_path}spline_interpolated.npy', [spectra_wav, spectra_rad])
-    np.save(f'{folder_path}corrected_datacube.npy', corrected_datacube)
-
-    print("Data saved to data/TempData")
+    np.savez_compressed(f'{data_folder_path}corrected_datacube', corrected_data = corrected_datacube)
+    print(f"Correction complete, no issues. Data saved to {data_folder_path}.")
 
     # # # Clear all variables
     column_averaged_spectra = None
--- smile/plot_config.py
+++ smile/plot_config.py
@@ -16,6 +16,22 @@ indian_pine_wavelength = np.load(indian_pine_array_filepath)
 # # Global Variables
 _, _, _, wavelength, _ = ldn(indian_pine_array_filepath, indian_pine_wavelength_filepath)
 
+# # QoL Functions
+def stretch_horizontal(to_be_stretched, target):
+    """
+    A linear transformation that stretches one array of any size, so it starts
+        and ends at the same points as the target array.
+
+    Args: 
+        to_be_stretched: the array that needs to be stretched
+        target: the array which length to_be_stretched needs to match
+
+    Outputs:
+        A 1D array, stretched input array
+    """
+    return np.array(to_be_stretched) * np.mean(np.diff(target)) + min(target)
+
+# # Plotting Functions
 # column_average_spectra
 def plot_column_average_spectra(save=True, row_to_plot = None):
     """Plots column average spectra. By default, it plots the whole picture of the 2D collapse result. However, it can also plot a single row of the 2D collapse result if plot_row is specified.
@@ -25,7 +41,7 @@ def plot_column_average_spectra(save=True, row_to_plot = None):
     Returns:
         None
     """
-    column_average_spectra = np.load(f'{DataFolder}column_averaged_spectra.npy')
+    column_average_spectra = np.load(f'{DataFolder}column_averaged_spectra.npz', mmap_mode='r')['cas']
 
     fig, ax = plt.subplots(1, 1, figsize=(9, 7))
     row_text = ''
@@ -44,25 +60,109 @@ def plot_column_average_spectra(save=True, row_to_plot = None):
         fig.savefig(f"{PlotFolder}ColumnAverageSpectra{row_text}.png")
 
 # create_ref_and_test_spectra
-def plot_ref_and_test_spectra(save=True, crop_range=None):
+def plot_resampled_ref_spectra(save=True, crop_range=None, to_be_plotted=None):
+    """Plots resampled reference spectra along with the shifts.
+    Args:
+        save (bool): If True, saves the plot to the current directory.
+        crop_range (tuple): If specified, crops the plot to the specified range.
+        to_be_plotted (str) or (int) or None: If input is 'refrence', plots the reference spectra. If input is any integer within the range of the number of columns, plots the test spectra of the specified column. If input is None, plots nothing.
+    Returns:
+        None
     """
-    """
-    crop_start, crop_end = crop_range
-    band_length = int((max(wavelength) - min(wavelength)) / main.g_num_of_bands)
-    crop_band_start = crop_start * band_length
-    crop_band_end = (crop_end + 1) * band_length
+    shift_extent = main.g_num_shifts_1D * main.g_shift_increment
+    shift_range = np.arange(-shift_extent, shift_extent, main.g_shift_increment)
 
+    if crop_range is not None:
+        crop_start, crop_end = crop_range
+        band_length = int((max(wavelength) - min(wavelength)) / main.g_num_of_bands)
+        crop_band_start = crop_start * band_length # TODO: add this
+        crop_band_end = (crop_end + 1) * band_length
+
+    # # Retrieve data
+    input_data = np.load(f'{DataFolder}column_averaged_spectra.npz',mmap_mode='r')['cas']
+    reference_and_test = np.load(f'{DataFolder}ref_and_test_spectra.npz', allow_pickle=True)
+
+    # # Unpack data
+    # Reference_spectra = (num of shifts, num of bands)
+    reference_, test_ = reference_and_test['ref'], reference_and_test['test']
+    reference_spectra, sensors_position_reference, srf_columns_reference = reference_
+    test_spectra, sensors_position_test, srf_columns_test = test_
+
+    # # Crop data
+    # cropped_reference = [i[crop_start:crop_end] for i in reference_spectra]
+    # cropped_test = [i[crop_start:crop_end] for i in test_spectra]
+
+    # # Plot data
+    fig, reference_plot = plt.subplots(1, 1, figsize=(25, 7))
+
+    reference_plot.set_xlabel('Wavelength (nm)', fontsize = 15)
+    reference_plot.set_ylabel('Radiance', fontsize = 15)
+
+    # Plot the original data
+    if main.MODTRAN_data is None:
+        reference_plot.plot(wavelength, input_data[0], label='First Column as Reference Spectra')
+        ref_ = "\n Reference: first comlumn"
+    else:
+        reference_plot.plot(wavelength, main.MODTRAN_data, label='RefSerence Spectra from MODTRAN')
+
+    fig_title = 'empty'
+
+    # Plot the results
+    if to_be_plotted == 'reference':
+        sensors_positions = stretch_horizontal(sensors_position_reference[0], wavelength)
+        fig_title = f"Reference Spectra with {main.g_num_shifts_1D} Shifts"
+        for i, ref_shift in enumerate(reference_spectra):
+            reference_plot.scatter(sensors_positions, ref_shift, label=f'shift {shift_range[i]}')
+            reference_plot.plot(np.linspace(min(wavelength), max(wavelength), main.g_num_of_bands*100), srf_columns_reference[i])
+    elif to_be_plotted < len(test_spectra):
+        # print(np.shape(sensors_position_test[to_be_plotted][0]), np.shape(test_spectra[to_be_plotted][0]))
+        sensors_positions = stretch_horizontal(sensors_position_test[to_be_plotted][0], wavelength)
+        reference_plot.scatter(sensors_positions, test_spectra[to_be_plotted][0], label=f'Test Spectra of Column {to_be_plotted}')
+        fig_title = f"Test Spectra of Column {to_be_plotted}"
+    elif to_be_plotted >= len(test_spectra):
+        raise UserWarning(f"Error: to_be_plotted is {to_be_plotted}, which is greater than the number of columns in the test spectra ({len(test_spectra)}).")
+    elif to_be_plotted is None:
+        raise UserWarning("Error: to_be_plotted is None. Please specify what to be plotted.")
+    
+    reference_plot.legend()
+    reference_plot.set_title(fig_title + ref_, fontsize = 15)
+    if save:
+        fig.savefig(f"{PlotFolder}RefAndTestSpectra.png")
+
+# spectral angle calculation
+
+def plot_spectral_angle(column_to_plot = None, save=True):
+    """Plots the spectral angle between the reference and test spectra.
+    Args:
+        column_to_plot (int): The column to plot the spectral angle of. If None, plots the all columns in the same figure.
+        save (bool): If True, saves the plot to the current directory.
+    Returns:
+        None
+    """
     # Retrieve data
-    ref, test = np.load(f'{DataFolder}ref_and_test_spectra.npy', mmap_mode='r')
-
-    # Unpack data
-    reference_spectra, sensors_position_reference, srf_columns_reference = ref
-    test_spectra, sensors_position_test, srf_columns_test = test
-
-    # Crop data
-    cropped_reference = [i[crop_start:crop_end] for i in reference_spectra]
-    cropped_test = [i[crop_start:crop_end] for i in test_spectra]
+    spectral_angle = np.load(f'{DataFolder}sa_deg.npz', allow_pickle=True)['sa_deg']
 
     # Plot data
-    fig, ref_and_test_plot = plt.subplots(1, 1, figsize=(9, 7))
+    fig, spectral_angle_plot = plt.subplots(1, 1, figsize=(9, 7))
 
+    if column_to_plot is None:
+        for i, sa in enumerate(spectral_angle):
+            spectral_angle_plot.plot(sa, label=f'Column {i}')
+            columns = "all columns"
+    elif column_to_plot < len(spectral_angle):
+        spectral_angle_plot.plot(spectral_angle[column_to_plot], label=f'Column {column_to_plot}')
+        columns = f"Column {column_to_plot}"
+    elif column_to_plot >= len(spectral_angle):
+        raise UserWarning(f"Error: column_to_plot is {column_to_plot}, which is greater than the number of columns in the test spectra ({len(spectral_angle)}).")
+
+    spectral_angle_plot.set_xlabel('Shifts', fontsize = 15)
+    spectral_angle_plot.set_ylabel('Spectral Angle (deg)', fontsize = 15)
+    spectral_angle_plot.set_title(f"Spectral Angle of {columns}")
+    spectral_angle_plot.grid()
+
+    if save:
+        fig.savefig(f"{PlotFolder}SpectralAngle.png")
+
+# plot_column_average_spectra()
+# plot_resampled_ref_spectra(save=True, to_be_plotted=100)
+# plot_spectral_angle()
