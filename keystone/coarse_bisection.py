d48a01fdfb7816be3dd3bf5993ce1dec6e6afec3
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-12-02 16:00:55 -0500

    Update coarse_bisection.py

--- keystone/coarse_bisection.py
+++ keystone/coarse_bisection.py
@@ -1,36 +1,44 @@
+# Author: Evan Song
+# This file contains a helper function to implement the coarse bisection technique
 
-import numpy as np
-import matplotlib.pyplot as plt
-import math
+from apply_keystone import apply_keystone
+from compute_sinc_fit_peak import compute_sinc_fit_peak
 
-from scipy import stats
-from IPython.display import clear_output
 def coarse_bisection(guess, imagex, adjustment, imagex1):
-  """
-  Args:
-  guess: our first initial polynomial guess for coarse bisection
-  imagex: image at band x
-  adjustment: amount we are adjusting the guess up and down by, to get 2 more guesses for coarse bisection (user defined)
-  imagex1: image at band x+1
+    """
+    Executes coarse bisection method to find the guess with the lowest phase shift.
+    
+    Args:
+    - guess: Initial polynomial guess for coarse bisection
+    - imagex: Image at band x
+    - adjustment: User-defined amount to adjust the guess up and down to obtain two more guesses for coarse bisection
+    - imagex1: Image at band x+1
 
-  Returns:
-  the guess with the lowest phase shift, in the form: tuple(guess, associated phase shift)
-  """
+    Returns:
+    - Tuple: (Guess with the lowest phase shift, Associated phase shift)
+    """
   
-  
-  up_adjusted_guess = guess + adjustment
-  down_adjusted_guess = guess - adjustment
-  adjusted_1 = apply_keystone(imagex1, up_adjusted_guess)
-  adjusted_2 = apply_keystone(imagex1, down_adjusted_guess)
-  adjusted_3 = apply_keystone(imagex1, guess)
-  shift_1 = compute_sinc_fit_peak(imagex, adjusted_1)
-  shift_2 = compute_sinc_fit_peak(imagex, adjusted_2)
-  shift_3 = compute_sinc_fit_peak(imagex, adjusted_3)
-  shift_list = [shift_1, shift_2, shift_3]
-  minimum = min(shift_list)
-  if minimum == shift_1:
-    return (up_adjusted_guess, shift_1)
-  elif minimum == shift_2:
-    return (down_adjusted_guess, shift_2)
-  else:
-    return (guess, shift_3)
+    # Adjust guesses
+    up_adjusted_guess = guess + adjustment
+    down_adjusted_guess = guess - adjustment
+    
+    # Compute phase shifts for adjusted guesses
+    adjusted_1 = apply_keystone(imagex1, up_adjusted_guess)
+    adjusted_2 = apply_keystone(imagex1, down_adjusted_guess)
+    adjusted_3 = apply_keystone(imagex1, guess)
+    
+    shift_1 = compute_sinc_fit_peak(imagex, adjusted_1)
+    shift_2 = compute_sinc_fit_peak(imagex, adjusted_2)
+    shift_3 = compute_sinc_fit_peak(imagex, adjusted_3)
+    
+    # Get the guess with the lowest phase shift
+    shift_list = [shift_1, shift_2, shift_3]
+    minimum = min(shift_list)
+    
+    # Return the guess with the lowest phase shift
+    if minimum == shift_1:
+        return (up_adjusted_guess, shift_1)
+    elif minimum == shift_2:
+        return (down_adjusted_guess, shift_2)
+    else:
+        return (guess, shift_3)
