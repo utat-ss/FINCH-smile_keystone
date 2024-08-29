3d4ce256fd6315e8458c8a2addcabbb1793fce86
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2024-08-29 16:58:43 -0400

    Update coarse_bisection_recursive.py

--- keystone/coarse_bisection_recursive.py
+++ keystone/coarse_bisection_recursive.py
@@ -3,30 +3,43 @@
 
 from coarse_bisection import coarse_bisection
 
-def coarse_bisection_recursive(imgx, imgx1, guess, shift, threshold, iteration, max_iteration):
+def coarse_bisection_recursive(
+    imgx, imgx1, guess, shift, threshold, iteration, max_iteration, adjustment
+):
     """
-    Applies a recursive coarse bisection method to refine a guess until a phase shift threshold is reached 
+    Applies a recursive coarse bisection method to refine a guess until a phase shift threshold is reached
     or maximum iterations are reached.
 
     Args:
-    - imgx: Image at band x
-    - imgx1: Image at band x+1
-    - guess: Initial guess for coarse bisection
-    - shift: Current phase shift value associated with the guess
-    - threshold: Threshold value for the phase shift, determines termination condition
-    - iteration: Current iteration count
-    - max_iteration: Maximum number of iterations allowed
+        imgx: Image at band x.
+        imgx1: Image at band x+1.
+        guess: Initial guess for coarse bisection.
+        shift: Current phase shift value associated with the guess.
+        threshold: Threshold value for the phase shift, determines termination condition.
+        iteration: Current iteration count.
+        max_iteration: Maximum number of iterations allowed.
+        adjustment: Adjustment value for bisection.
 
     Returns:
-    - Updated guess or the last calculated guess if the threshold or maximum iterations are reached
+        Updated guess or the last calculated guess if the threshold or maximum iterations are reached.
     """
-
+    initial_shift = shift
     # Termination condition: phase shift below threshold or reached maximum iterations
     if shift <= threshold or iteration >= max_iteration:
         return guess
     else:
         # Refine the guess using coarse bisection method
         guess, shift = coarse_bisection(guess, imgx, adjustment, imgx1)
-        
+        if shift == initial_shift:
+            adjustment = adjustment / 2
         # Recursive call with updated guess and shift values
-        return coarse_bisection_recursive(imgx, imgx1, guess, shift, threshold, iteration + 1, max_iteration)
+        return coarse_bisection_recursive(
+            imgx,
+            imgx1,
+            guess,
+            shift,
+            threshold,
+            iteration + 1,
+            max_iteration,
+            adjustment,
+        )
