d2bd9d0e9e4f322a398ec87c35aee54a3e73c1d3
Shakoor <shakoorshaik7@gmail.com>
2024-11-30 11:33:53 -0500

    Update extract_from_MODTRAN.py

--- smile/extract_from_MODTRAN.py
+++ smile/extract_from_MODTRAN.py
@@ -26,38 +26,48 @@ def extract_from_MODTRAN(
     Outputs:
         np.array consisting of x and y-axis data points, wavelength in nm on x-axis and transmittance on the y-axis
     """
-    f = open(json_file, "r")
-    data = js.load(f)
-    json = data["datasets"][dataset_no]
-    key = list(json.keys())[0]
-    ind = 0
-    for i in range(len(json[key]["roots"]["references"])):
-        if "data" in json[key]["roots"]["references"][i]["attributes"]:
-            ind = i
+    try:
 
-    xpoint = np.array(json[key]["roots"]["references"][ind]["attributes"]["data"]["x"])
-    ypoints = np.array(json[key]["roots"]["references"][ind]["attributes"]["data"]["y"])
-    xpoints = np.array([d * 1000 for d in xpoint])
+        with open(json_file, "r") as f:
+            data = js.load(f)
 
-    n1, n2 = start_wavelength, end_wavelength
-    xp_temp = np.array([d for d in xpoints if n1 < d < n2])
-    pos = [list(xpoints).index(d) for d in list(xpoints) if n1 < d < n2]
-    optimised_y = []
-    optimised_x = []
-    for i in pos:
-        if ypoints[i] < 0.91:
-            optimised_y.append(ypoints[i] - deeper_by)
-        else:
-            optimised_y.append(ypoints[i])
-    x_of_min = xp_temp[np.argmin(ypoints[pos])]
-    for point in xp_temp:
-        if point < x_of_min:
-            optimised_x.append(point - wider_by)
-        elif point > x_of_min:
-            optimised_x.append(point + wider_by)
-        else:
-            optimised_x.append(point)
-    yp = np.array(optimised_y)
-    xp = np.array(optimised_x)
+        json = data["datasets"][dataset_no]
+        key = list(json.keys())[0]
+        ind = 0
+        for i in range(len(json[key]["roots"]["references"])):
+            if "data" in json[key]["roots"]["references"][i]["attributes"]:
+                ind = i
 
-    return xp, yp
+        xpoint = np.array(json[key]["roots"]["references"][ind]["attributes"]["data"]["x"])
+        ypoints = np.array(json[key]["roots"]["references"][ind]["attributes"]["data"]["y"])
+        xpoints = np.array([d * 1000 for d in xpoint])
+
+        n1, n2 = start_wavelength, end_wavelength
+        xp_temp = np.array([d for d in xpoints if n1 < d < n2])
+        pos = [list(xpoints).index(d) for d in list(xpoints) if n1 < d < n2]
+        optimised_y = []
+        optimised_x = []
+        for i in pos:
+            if ypoints[i] < 0.91:
+                optimised_y.append(ypoints[i] - deeper_by)
+            else:
+                optimised_y.append(ypoints[i])
+        x_of_min = xp_temp[np.argmin(ypoints[pos])]
+        for point in xp_temp:
+            if point < x_of_min:
+                optimised_x.append(point - wider_by)
+            elif point > x_of_min:
+                optimised_x.append(point + wider_by)
+            else:
+                optimised_x.append(point)
+        yp = np.array(optimised_y)
+        xp = np.array(optimised_x)
+
+        return xp, yp
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {json_file} not found.")
+    except KeyError as e:
+        raise KeyError(f"Missing key in JSON: {e}")
+    except Exception as e:
+        raise RuntimeError(f"An error occurred: {e}")
